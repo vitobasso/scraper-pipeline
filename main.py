d5b6380f1df0fa89977c97e0332c94c855d33577
Victor Basso <victor@veganhive.com>
2025-05-31 11:16:26 -0300

    proxies, str quotes

--- main.py
+++ main.py
@@ -4,25 +4,20 @@ from extract_googleai import extract
 import datetime
 
 def screenshot_tipranks(ticker: str):
-    screenshot_template('tipranks', ticker, f"https://www.tipranks.com/etf/{ticker.lower()}/forecast")
+    screenshot_template('tipranks', ticker, f'https://www.tipranks.com/etf/{ticker.lower()}/forecast')
 
 def screenshot_tradingview(ticker: str):
-    screenshot_template('tradingview', ticker, f"https://tradingview.com/symbols/{ticker.upper()}/forecast/")
+    screenshot_template('tradingview', ticker, f'https://tradingview.com/symbols/{ticker.upper()}/forecast/')
 
 def screenshot_template(site: str, ticker: str, url: str):
     timestamp = datetime.datetime.now().strftime('%Y%m%dT%H%M%S')
-    filename = f"{site}-{ticker}-{timestamp}.png"
-    proxy = proxies.pick()
-    print(f'screenshot, url: {url}, filename: {filename}, proxy: {proxy}')
-    try:
-        take_screenshot(url, filename, proxy)
-        proxies.report(proxy, True)
-    except Exception as e:
-        proxies.report(proxy, False)
+    filename = f'{site}-{ticker}-{timestamp}.png'
+    print(f'taking screenshot, url: {url}, filename: {filename}')
+    proxies.run_task(lambda proxy: take_screenshot(url, filename, proxy))
 
 if __name__ == '__main__':
     for i in range(10):
-        screenshot_tipranks("qqq")
-        screenshot_tradingview("BMFBOVESPA-PETR4")
+        screenshot_tipranks('qqq')
+        screenshot_tradingview('BMFBOVESPA-PETR4')
     # extract("screenshots/tradingview-BMFBOVESPA-PETR4-20250530T184949.png")
     # extract("screenshots/tipranks-qqq-20250530T180430.png")
--- proxies.py
+++ proxies.py
@@ -2,27 +2,36 @@ from tinydb import TinyDB, Query
 import datetime
 from random import randrange, choice
 
-with open("resources/proxies.txt") as f:
+with open('resources/proxies.txt') as f:
     proxies = f.read().splitlines()
 
 db = TinyDB('proxies.json')
 
 for line in proxies:
     db.upsert({'proxy': line}, Query().proxy == line)
+
+def run_task(task):
+    proxy = pick()
+    try:
+        task(proxy)
+        report(proxy, True)
+    except Exception as e:
+        report(proxy, False)
     
 def report(proxy: str, succeeded: bool):
+    print(f'proxy {proxy} {'succeeded' if succeeded else 'failed'}')
     if records := db.search(Query().proxy == proxy):
         record = records[0]
-        record["count_usages"] = record.get("count_usages", 0) + 1
-        record["last_used"] = datetime.datetime.now().timestamp()
+        record['count_usages'] = record.get('count_usages', 0) + 1
+        record['last_used'] = datetime.datetime.now().timestamp()
         if succeeded:
-            record["count_successes"] = record.get("count_successes", 0) + 1
-            record["last_succeeded"] = record["last_used"]
+            record['count_successes'] = record.get('count_successes', 0) + 1
+            record['last_succeeded'] = record['last_used']
         db.update(record, Query().proxy == proxy)
 
 def pick():
     record = _pick()
-    return record["proxy"] if record else None
+    return record['proxy'] if record else None
 
 def _pick():
     all_proxies = db.all()
@@ -35,22 +44,22 @@ def _pick():
         return _pick_oldest(healthy) or _pick_random(never_used) or _pick_oldest(retriable)
 
 def _pick_oldest(_list):
-    return sorted(_list, key=lambda x: x["last_used"], reverse=True)[0] if _list else None
+    return sorted(_list, key=lambda x: x['last_used'], reverse=True)[0] if _list else None
 
 def _pick_random(_list):
     return choice(_list) if _list else None
 
 def _is_never_used(record):
-    return record.get("last_used") is None
+    return record.get('last_used') is None
 
 def _is_healthy(record):
     day_secs = 60 * 60 * 24
-    return not _is_never_used(record) and record.get("last_used") - record.get("last_succeeded", 0) < day_secs
+    return not _is_never_used(record) and record.get('last_used') - record.get('last_succeeded', 0) < day_secs
 
 def _is_retriable(record):
     if _is_never_used(record) or _is_healthy(record):
         return False
     else:
-        count_failures = record.get("count_usages", 0) - record.get("count_successes", 0)
-        ratio_success = record.get("count_successes", 0) / record.get("count_usages", 0)
+        count_failures = record.get('count_usages', 0) - record.get('count_successes', 0)
+        ratio_success = record.get('count_successes', 0) / record.get('count_usages', 0)
         return count_failures < 10 or ratio_success > .1
