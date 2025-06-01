876d1d0eafd484113acd5f6eba55bbe425ee7d37
Victor Basso <victor@veganhive.com>
2025-06-01 15:59:27 -0300

    add investidor10

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from src.stocks import stocks_screenshot
-from src.stocks.stocks_image_scrape import extract
+from src.stocks.stocks_image_scrape import extract_analysis
+from src.stocks.stocks_image_scrape import extract_fundamentals
 
 if __name__ == '__main__':
     stocks_screenshot.run_tasks()
-    # extract("screenshots/tradingview-BMFBOVESPA-PETR4-20250530T184949.png")
-    # extract("screenshots/tipranks-qqq-20250530T180430.png")
+    # extract_analysis("output/screenshots/valid/tradingview-BMFBOVESPA-PETR4-20250530T184949.png")
+    # extract_analysis("output/screenshots/valid/tipranks-qqq-20250530T180430.png")
+    # extract_fundamentals("output/screenshots/valid/investidor10-bbas3-20250601T155718.png")
