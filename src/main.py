dc2e39b4b5fe303022a678136b2d5beda4d5b32a
Victor Basso <vitobasso@gmail.com>
2025-06-04 16:52:31 -0300

    refactor screenshot, remove parallel tasks

--- src/main.py
+++ src/main.py
@@ -1,19 +1,18 @@
-from src.stocks import stocks_screenshot
+# from src.core.proxies import random_proxy
+# import src.stocks.ticker_screenshot as ticker_screenshot
 # from src.stocks.stocks_image_scrape import extract_analysis
 # from src.stocks.stocks_image_scrape import extract_fundamentals
 # from src.stocks.screening_sheet import create_spreadsheet
-import src.stocks.statusinvest as statusinvest
-import src.stocks.b3idiv as b3idiv
-# from src.core.proxies import random_proxy
+# import src.stocks.statusinvest as statusinvest
+# import src.stocks.b3idiv as b3idiv
 
 if __name__ == '__main__':
-    # stocks_screenshot.run_tasks()
+    # proxy = random_proxy()
+    # ticker_screenshot.screenshot_yahoo('bbas3.sa')
     # statusinvest.sync_download()
     # b3idiv.sync_download()
     # extract_analysis("output/screenshots/valid/tradingview-BMFBOVESPA-PETR4-20250530T184949.png")
     # extract_analysis("output/screenshots/valid/tipranks-qqq-20250530T180430.png")
     # extract_fundamentals("output/screenshots/valid/investidor10-bbas3-20250601T155718.png")
     # create_spreadsheet()
-    # proxy = random_proxy()
     pass
-
