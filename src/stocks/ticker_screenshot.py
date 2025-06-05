501e5614404fdb07c96c9385bb619486633bab03
Victor Basso <victor@veganhive.com>
2025-06-05 16:34:51 -0300

    simplywall, proxy name

--- src/stocks/ticker_screenshot.py
+++ src/stocks/ticker_screenshot.py
@@ -8,26 +8,35 @@ output_dir = 'output/screenshots'
 after_load_timeout = config.get('screenshot.browser.after_load_timeout')
 
 def screenshot_tipranks(ticker: str, ticker_type: str):
-    sync_screenshot('tipranks', ticker, f'https://www.tipranks.com/{ticker_type}/{ticker}/forecast')
+    sync_screenshot_ticker('tipranks', ticker, f'https://www.tipranks.com/{ticker_type}/{ticker}/forecast')
 
 def screenshot_tradingview(ticker: str):
-    sync_screenshot('tradingview', ticker, f'https://tradingview.com/symbols/{ticker}/forecast/')
+    sync_screenshot_ticker('tradingview', ticker, f'https://tradingview.com/symbols/{ticker}/forecast/')
 
 def screenshot_yahoo(ticker: str):
-    sync_screenshot('yahoo', ticker, f'https://finance.yahoo.com/quote/{ticker}/analysis')
+    sync_screenshot_ticker('yahoo', ticker, f'https://finance.yahoo.com/quote/{ticker}/analysis')
 
 def screenshot_yahoo_br(ticker: str):
-    sync_screenshot('yahoo', ticker, f'https://finance.yahoo.com/quote/{ticker}.sa/analysis')
+    sync_screenshot_ticker('yahoo', ticker, f'https://finance.yahoo.com/quote/{ticker}.sa/analysis')
 
 def screenshot_investidor10(ticker: str):
-    sync_screenshot('investidor10', ticker, f'https://investidor10.com.br/acoes/{ticker}/')
+    sync_screenshot_ticker('investidor10', ticker, f'https://investidor10.com.br/acoes/{ticker}/')
 
-def sync_screenshot(site_name: str, ticker: str, url: str):
-    asyncio.run(screenshot(site_name, ticker, url))
+def screenshot_simplywall():
+    sync_screenshot('simplywall.st', 'https://simplywall.st/stocks/br/top-gainers')
 
-async def screenshot(site_name: str, ticker: str, url: str):
+def sync_screenshot_ticker(site_name: str, ticker: str, url: str):
+    asyncio.run(screenshot_ticker(site_name, ticker, url))
+
+def sync_screenshot(site_name: str, url: str):
+    asyncio.run(screenshot(site_name, url))
+
+async def screenshot_ticker(site_name: str, ticker: str, url: str):
+    await screenshot(f'{site_name}-{ticker}.png', url)
+
+async def screenshot(key: str, url: str):
     timestamp = datetime.datetime.now().strftime('%Y%m%dT%H%M%S')
-    filename = f'{site_name}-{ticker}-{timestamp}.png'
+    filename = f'{key}-{timestamp}.png'
     temp_path = f'{output_dir}/awaiting-validation/{filename}'
     proxy = random_proxy()
     print(f'taking screenshot, url: {url}, filename: {filename}, proxy: {proxy}')
