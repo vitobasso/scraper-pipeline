b382bee15f77bfd4dd5161b6692c84e835418b3d
Victor Basso <victor@veganhive.com>
2025-06-04 22:22:25 -0300

    polish extracted json and error msgs

--- src/stocks/ticker_screenshot.py
+++ src/stocks/ticker_screenshot.py
@@ -1,9 +1,8 @@
-import datetime, asyncio, sys
-import re
-
+import re, datetime, asyncio, sys
+from src.core.config import config
 from src.core.proxies import random_proxy
 from src.core.browser_session import browser_page
-from src.core.config import config
+from playwright._impl._errors import TimeoutError, Error as PlaywrightError
 
 output_dir = 'output/screenshots'
 after_load_timeout = config.get('screenshot.browser.after_load_timeout')
@@ -37,7 +36,13 @@ async def _screenshot(proxy: str, url: str, path: str):
             await page.wait_for_timeout(after_load_timeout)
             await page.screenshot(path=path, full_page=True, animations='disabled')
     except Exception as e:
-        print(f'   failed: {_error_type(e)}', sys.stderr)
+        print(f'   failed: {_error_type(e)}', file=sys.stderr)
 
 def _error_type(e: Exception):
-    return re.match(r'(\w+) .*', type(e).__name__).group(1)
+    if isinstance(e, TimeoutError):
+        return type(e).__name__
+    if isinstance(e, PlaywrightError):
+        match = re.search(r'ERR_\w+', str(e))
+        return match.group(0) if match else str(e)
+    else:
+        return str(e)
