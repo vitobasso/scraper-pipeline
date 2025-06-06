34a602968cc916fe1d2654442f65e25d9a3ea45f
Victor Basso <vitobasso@gmail.com>
2025-06-06 15:04:31 -0300

    polish

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
-from src.flows import yahoo
 import scheduler
+from src.flows import yahoo, tradingview
+from src.spreadsheet.screening_sheet import create_spreadsheet
 
 if __name__ == '__main__':
-    for i in range(100):
-        scheduler.schedule_next(yahoo.flow())
+    # for i in range(100):
+        # scheduler.schedule_next(yahoo.flow())
+        # scheduler.schedule_next(tradingview.flow())
     # proxy = random_proxy()
     # yahoo.compile_data()
     # yahoo.screenshot('cmig4')
+    create_spreadsheet()
     pass
