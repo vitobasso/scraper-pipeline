aa28fa5917aadb0e847d1ed8091b8143cb89233d
Victor Basso <victor@veganhive.com>
2025-06-06 20:38:37 -0300

    tradingview: focused screenshot

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ from src.flows import yahoo, tradingview
 from src.spreadsheet.screening_sheet import create_spreadsheet
 
 if __name__ == '__main__':
-    # for i in range(100):
+    for i in range(100):
         # scheduler.schedule_next(yahoo.flow())
-        # scheduler.schedule_next(tradingview.flow())
+        scheduler.schedule_next(tradingview.flow())
     # proxy = random_proxy()
     # yahoo.compile_data()
     # yahoo.screenshot('cmig4')
