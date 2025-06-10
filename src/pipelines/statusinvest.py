477f8a9acb82bdbc1259687e9170dee287a223e4
Victor Basso <victor@veganhive.com>
2025-06-10 16:58:00 -0300

    fix statusinvest

--- src/pipelines/statusinvest.py
+++ src/pipelines/statusinvest.py
@@ -1,7 +1,7 @@
-import asyncio, csv
+import asyncio, csv, re
 from src.config import output_root
-from src.common.util import mkdir, timestamp
-from src.scheduler import Pipeline, seed_task
+from src.common.util import mkdir, timestamp, all_files
+from src.scheduler import Pipeline, seed_task, seed_progress
 from src.services.proxies import random_proxy
 from src.services.browser import page_goto, click, click_download
 
@@ -14,7 +14,8 @@ def pipeline() -> Pipeline:
         'name': name,
         'tasks': [
             seed_task(sync_download, output_dir),
-        ]
+        ],
+        'progress': seed_progress(output_dir)
     }
 
 
@@ -36,9 +37,13 @@ async def _download(proxy: str, path: str):
 
 
 def to_spreadsheet():
-    with open('output/20250605/downloads/statusinvest-20250602T214159.csv', 'r') as file:  # TODO dynamic path
+    files = all_files(output_dir)
+    if files:
+        file = files[0]
         rows = [row for row in csv.reader(file, delimiter=';')]
         return _clean_data(rows)
+    else:
+        return []
 
 
 def _clean_data(data):
--- src/services/google_sheets.py
+++ src/services/google_sheets.py
@@ -1,18 +1,29 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name("gcp-secret.json", scope)
-client = gspread.authorize(creds)
+_client = None
+
+
+def _get_client():
+    if not _client:
+        _init()
+    return _client
+
+
+def _init():
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name("gcp-secret.json", scope)
+    global _client
+    _client = gspread.authorize(creds)
 
 
 def copy_file(id, new_name: str):
-    original = client.open_by_url(f'https://docs.google.com/spreadsheets/d/{id}')
+    original = _get_client().open_by_url(f'https://docs.google.com/spreadsheets/d/{id}')
     original_permission = next(
         (perm for perm in original.list_permissions() if perm.get('role') == 'owner'),
         None  # Default if not found
     )
-    new_file = client.copy(original.id, title=new_name)
+    new_file = _get_client().copy(original.id, title=new_name)
     new_permission = new_file.share(email_address=original_permission['emailAddress'], perm_type='user', role='writer',
                                     notify=False)
     new_file.transfer_ownership(new_permission.json()['id'])
