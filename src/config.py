f4b11c469f5a5483351dd55cb7ffe991ee586011
Victor Basso <vitobasso@gmail.com>
2025-07-01 21:20:04 -0300

    config output_root

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ proxies_url = "https://cdn.jsdelivr.net/gh/proxifly/free-proxy-list@main/proxies
 # proxies_url = "https://cdn.jsdelivr.net/gh/proxifly/free-proxy-list@main/proxies/protocols/socks5/data.txt"
 # proxies_url = "https://cdn.jsdelivr.net/gh/proxifly/free-proxy-list@main/proxies/countries/US/data.txt"
 
-output_root = "output/20250621"
+output_root = "output/20250701"
 
 # visual_llm_model = "gemini-1.5-flash"
 # visual_llm_model = "gemini-2.0-flash-lite" # tradingview and tipranks work
