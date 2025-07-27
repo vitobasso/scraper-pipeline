7787eb31ee48ece297b70de2b8a2735261cfce5f
Victor Basso <vitobasso@gmail.com>
2025-07-27 17:07:15 -0300

    config reordered

--- src/config.py
+++ src/config.py
@@ -1,16 +1,15 @@
+output_root = "output/20250701"
+error_limit = 40
+
 proxies_url = "https://cdn.jsdelivr.net/gh/proxifly/free-proxy-list@main/proxies/protocols/socks4/data.txt"
 # proxies_url = "https://cdn.jsdelivr.net/gh/proxifly/free-proxy-list@main/proxies/protocols/socks5/data.txt"
 # proxies_url = "https://cdn.jsdelivr.net/gh/proxifly/free-proxy-list@main/proxies/countries/US/data.txt"
-
-output_root = "output/20250701"
 
 # visual_llm_model = "gemini-1.5-flash"
 # visual_llm_model = "gemini-2.0-flash-lite" # tradingview and tipranks work
 # visual_llm_model = "gemini-2.0-flash"
 visual_llm_model = "gemini-2.5-flash-preview-05-20"  # investidor10 works
 # model: "gemini-2.5-pro-preview-05-06"
-
-error_limit = 40
 
 google_dir = "1HjnwNHNxI17L9IFNhLt2Gzsj4o8IcJ9W"
 google_template_acoes = "1eWwqMZr4PeuH5siVoICz_XPcWVE8dGEgx_jKaarZ_4Y"  # TODO export to file and make it a local resource?
