5433468c868227a758309f17836fd8e5fd8aa71d
Victor Basso <victor@veganhive.com>
2025-08-04 21:47:06 -0300

    yahoo_recommendations

--- src/main.py
+++ src/main.py
@@ -6,21 +6,22 @@ csv_acoes_br = 'input/ticker-list/acoes-br.csv'
 
 if __name__ == '__main__':
     pipes = [
-        # fundamentus_fiis.pipeline(),
-        # investidor10.pipeline(csv_acoes_br),
-        # simplywall.pipeline(csv_acoes_br),
-        # statusinvest.pipeline(),
-        # # tipranks.pipeline('input/ticker-list/stocks-us.csv'),
-        # tradingview.pipeline(csv_acoes_br),
-        # yahoo.pipeline(csv_acoes_br),
+        fundamentus_fiis.pipeline(),
+        investidor10.pipeline(csv_acoes_br),
+        simplywall.pipeline(csv_acoes_br),
+        statusinvest.pipeline(),
+        # tipranks.pipeline('input/ticker-list/stocks-us.csv'),
+        tradingview.pipeline(csv_acoes_br),
+        yahoo.pipeline(csv_acoes_br),
         yahoo_chart.pipeline(csv_acoes_br),
+        yahoo_recomendations.pipeline(csv_acoes_br),
     ]
 
-    # report(pipes)
+    report(pipes)
     for i in range(200):
         for p in pipes:
             p.schedule_next()
-    # report(pipes)
+    report(pipes)
 
     # statusinvest_carteira_xlsx.import_all()
     # acoes_br.create_spreadsheet()
---
+++ src/pipelines/yahoo_recomendations.py
@@ -0,0 +1,60 @@
+import json, yfinance
+from src.config import output_root
+from src.common.util import mkdir, timestamp
+from src.common.validate_data import validate_schema, input_dir as validate_data_input, valid_data_dir
+from src.scheduler import Pipeline, line_task, file_task, line_progress
+
+from src.services.proxies import random_proxy
+
+name = 'yahoo_recommendations'
+output_dir = mkdir(f'{output_root}/{name}')
+data_dir = mkdir(f'{output_dir}/data/awaiting-validation')
+completed_dir = valid_data_dir(output_dir)
+
+
+def pipeline(input_path: str):
+    return Pipeline(
+        name=name,
+        tasks=[
+            line_task(call_api, input_path, output_dir),
+            file_task(validate_data, validate_data_input(output_dir)),
+        ],
+        progress=line_progress(input_path, output_dir)
+    )
+
+
+def call_api(ticker):
+    proxy = random_proxy()
+    path = f'{data_dir}/{ticker}-{timestamp()}.json'
+    print(f'scraping, ticker: {ticker}, path: {path}, proxy: {proxy}')
+    try:
+        ticker_obj = yfinance.Ticker(f'{ticker}.SA')
+        data = ticker_obj.get_recommendations(proxy=proxy).to_dict()
+        with open(path, 'w') as f:
+            json.dump(data, f)
+    except Exception as e:
+        return [str(e)]
+
+
+def validate_data(path: str):
+    schema = {
+        "period": {
+            "0": str,
+        },
+        "strongBuy": {
+            "0": int,
+        },
+        "buy": {
+            "0": int,
+        },
+        "hold": {
+            "0": int,
+        },
+        "sell": {
+            "0": int,
+        },
+        "strongSell": {
+            "0": int,
+        }
+    }
+    validate_schema(path, schema, output_dir)
