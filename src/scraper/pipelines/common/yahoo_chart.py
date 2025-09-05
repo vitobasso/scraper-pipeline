64400e420ee1427e97b7697d4d95837c07639d49
Victor Basso <vitobasso@gmail.com>
2025-09-05 19:21:10 -0300

    yahoo chart variation

--- src/scraper/pipelines/common/yahoo_chart.py
+++ src/scraper/pipelines/common/yahoo_chart.py
@@ -34,10 +34,32 @@ def call_api(pipe: Pipeline, ticker: str):
         log(str(e), ticker, pipe)
 
 
-validator = lambda data: ["array is empty"] if not data else []
+# business days
+MONTH_DAYS = 21
+YEAR_DAYS = 252
 
-normalize = lambda raw: {
-    "1mo": raw[-21:],
-    "1y": [v for i, v in enumerate(raw[-252:]) if i % 5 == 0],
-    "5y": [v for i, v in enumerate(raw) if i % 20 == 0],  # TODO smoothen the edges
-}
+# raw data expected to have 5 years of data in days, with 10% tolerance
+TOLERANCE_MIN = 5 * YEAR_DAYS * 0.9
+TOLERANCE_MAX = 5 * YEAR_DAYS * 1.1
+
+
+def validator(data):
+    if not data:
+        return ["array is empty"]
+    elif len(data) < TOLERANCE_MIN or len(data) > TOLERANCE_MAX:
+        return [f"expected between {TOLERANCE_MIN} and {TOLERANCE_MAX} days of data, got {len(data)}"]
+    else:
+        return []
+
+
+def normalize(raw):
+    avg = lambda start, end: sum(raw[start:end]) / (end - start)
+    var = lambda a1, a2, b1, b2: (avg(b1, b2) - avg(a1, a2)) / avg(a1, a2)
+    return {
+        "1mo_series": raw[-21:],
+        "1y_series": [v for i, v in enumerate(raw[-252:]) if i % 5 == 0],
+        "5y_series": [v for i, v in enumerate(raw) if i % 20 == 0],
+        "1mo_variation": var(-MONTH_DAYS - 1, -MONTH_DAYS + 1, -3, -1),
+        "1y_variation": var(-YEAR_DAYS - 12, -YEAR_DAYS + 12, -25, -1),
+        "5y_variation": var(0, 100, -100, -1),
+    }
